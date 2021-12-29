900aed1ed2f6d4a9c448880c167cc7bdc602fd57
Tran Van Quy <quytvgcd17298@fpt.edu.vn>
2021-12-29 22:32:40 +0700

    Setup and use React Router v5.2(newer version can use Switch in Router)

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import Navbar from './Components/layout/Navbar';
 import Users from './Components/users/Users';
 import axios from 'axios';
 import Search from './Components/users/Search';
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Fragment } from 'react/cjs/react.production.min';
+import About from './Components/pages/About';
+import NotFound from './Components/pages/NotFound';
+import {Switch} from "react-router-dom";
+ 
 class App extends Component {
   state = {
     usersData: [],
@@ -31,19 +36,38 @@ class App extends Component {
 
   render() {
   return (
+    <Router>
     <div>
       <Navbar title = {this.state.title}/>
       <div className="container">
-      <Search 
-      searchUsers = {this.searchUsers}
-      clearHandle = {this.clearHandle}
-      usersData = {this.state.usersData}
-      ></Search>
-        <Users 
-        showLoading = {this.state.showLoading}
-        usersData = {this.state.usersData}></Users>
+      <Switch>
+        <Route
+        exact
+        path="/"
+        render={() => (
+          <Fragment>
+            <Search 
+              searchUsers = {this.searchUsers}
+              clearHandle = {this.clearHandle}
+              usersData = {this.state.usersData}>
+            </Search>
+            <Users 
+              showLoading = {this.state.showLoading}
+              usersData = {this.state.usersData}>
+            </Users>
+          </Fragment>
+        )}
+        ></Route>
+        <Route
+        exact path ="/About" component ={About}
+        ></Route>
+        <Route
+        path = "/" component={NotFound}
+        ></Route>
+        </Switch>
         </div>
     </div>
+    </Router>
     );
   }
 }
---
+++ src/Components/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div className='container'>
+            <h2>This is About us</h2>
+            <p>Search any githud account you want by name</p>
+        </div>
+    )
+}
